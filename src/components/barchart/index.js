269cc0acf7b79f8b210c9e435b0a321777e96ac7
Broad, Oliver <Oliver.Broad@aperto.com>
2018-04-12 12:20:03 +0200

    Add global ChartWrapper class

--- src/components/barchart/index.js
+++ src/components/barchart/index.js
@@ -1,18 +1,7 @@
-/**
- * Description of BarChartWrapper.
- *
- * @module BarChartWrapper
- * @version v1.0.0
- *
- * @author Oliver Broad
- */
-
-// Imports
+import ChartWrapper from '../../global/ChartWrapper';
 import * as d3 from 'd3';
-import { merge } from 'lodash';
 
-class BarChart { // Should we have a parent class that this component extends? like => class BarChart extends d3Component / d3WrapperComponent or something like that?
-
+class BarChart extends ChartWrapper {
 	constructor(options) {
 		let defaultOptions = {
 			chartContainer: '[data-js-item="chart"]',
@@ -36,54 +25,7 @@ class BarChart { // Should we have a parent class that this component extends? l
 			}
 		};
 
-        this.options = merge(defaultOptions, options);
-	}
-
-	initialize() {
-        this.chartContainer = document.querySelector(this.options.chartContainer);
-        if (this.options.standalone) {
-			this.addData();
-			this.displayChart();
-		}
-	}
-
-	setXKey(key) {
-		this.checkKey(key);
-		this.xKey = key;
-		this.xData = this.data.map(d => d[this.xKey]);
-	}
-
-	setYKey(key) {
-		this.checkKey(key);
-		this.yKey = key;
-		this.yData = this.data.map(d => +d[this.yKey]);
-	}
-
-	addData(data = this.options.data) {
-        if (typeof data === 'undefined') {
-            throw new Error('The data you have passed is undefined!');
-        }
-
-        this.data = data;
-
-		if (this.options.xKey || this.options.yKey) {
-			this.keys = Object.keys(data[0]);
-			
-			this.options.xKey && this.setXKey(this.options.xKey);
-			this.options.yKey && this.setYKey(this.options.yKey);
-		}
-	}
-
-	checkKey(key) {
-        if (this.keys.constructor !== Array) {
-            throw new Error('No data keys defined.')
-        }
-        
-        let keyExists = this.keys.indexOf(key) != -1;
-
-		if (!keyExists) {
-			throw new Error(`The key "${key}" you have provided is not available in the data.`);
-		}
+		super(defaultOptions, options);
 	}
 
 	getXScale(data) {
@@ -204,15 +146,6 @@ class BarChart { // Should we have a parent class that this component extends? l
 			.attr('height', d => {
 				return Math.abs(this.calculateY(d) - this.yScale(0));
 			});
-	}
-
-	getDefaultWidth() {
-		if (!!this.chartContainer) {
-			return this.chartContainer.offsetWidth;
-		} else {
-			console.error('No chart container found. ' +
-						 'Specify a selector or add a div with the default "data-js-item=\"chart\".')
-		}
 	}
 }
 
